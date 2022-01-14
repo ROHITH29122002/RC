26472a25efbf3394c42180a87038f0f5821c8ee8
Rohith Challa <rohithsince2002@gmail.com>
2022-01-14 11:52:19 +0530

    define styles for navbar.

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+const nbright = document.querySelector(".nb-right");
+const nbopen = document.querySelector(".nb-open");
+const nbclose = document.querySelector(".nb-close");
+
+
+
+if(localStorage.getItem('name')){
+}
+else{
+    document.querySelector('#nb-logout').style.display="none";
+}
+nbopen.addEventListener('click',show);
+nbclose.addEventListener('click',close);
+
+function show(){
+    nbright.style.display = 'flex';
+    nbright.style.top = '0';
+}
+
+function close(){
+    nbright.style.top = '-100%';
+}
