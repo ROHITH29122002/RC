7c55401f52e59db14e4696563290af43aefb8209
Rohith Challa <rohithsince2002@gmail.com>
2022-01-15 12:25:37 +0530

    define javascript for form.

--- javascript/index.js
+++ javascript/index.js
@@ -5,11 +5,27 @@ const nbclose = document.querySelector(".nb-close");
 
 if(localStorage.getItem('name')){
     document.querySelector('#nameform-container').style.display="none";
-    document.querySelector('#nb-logout').style.display="block";
+    nameplacer();
 }
 else{
     document.querySelector('#nb-logout').style.display="none";
     document.querySelector('.name-container').style.display="none";
+    document.querySelector('#name-submit').disabled="true";
+    document.getElementById('name-input').onkeyup = function(){
+        if(document.querySelector('#name-input').value.length>0){
+            document.querySelector('#name-submit').disabled=false;
+            document.querySelector('#name-submit').classList.add('name-submit-green');
+        }
+        else{
+            document.querySelector('#name-submit').disabled=true;
+            document.querySelector('#name-submit').classList.remove('name-submit-green');
+        }
+    }
+    document.querySelector('#name-form').onsubmit = function (){
+        var name = document.querySelector('#name-input').value;
+        localStorage.setItem('name', name);
+        location.reload();
+    }
 }
 nbopen.addEventListener('click',show);
 nbclose.addEventListener('click',close);
@@ -26,4 +42,8 @@ function close(){
 document.querySelector('#nb-logout').onclick = function (){
     localStorage.removeItem('name');
     location.reload();
+}
+
+function nameplacer(){
+    document.querySelector('.hero-heading').innerHTML=`WELCOME , ${localStorage.getItem('name')}`;
 }
