ffbc0a59bd3ac9a07b1fef1d8bcecd11e57c50cc
Rohith Challa <rohithsince2002@gmail.com>
2022-01-19 14:28:55 +0530

    define styles for nav footer in about.

--- javascript/about.js
+++ javascript/about.js
@@ -1,3 +1,21 @@
+const nbright = document.querySelector(".nb-right");
+const nbopen = document.querySelector(".nb-open");
+const nbclose = document.querySelector(".nb-close");
+
+nbopen.addEventListener('click',show);
+nbclose.addEventListener('click',close);
+
+function show(){
+    nbright.style.display = 'flex';
+    nbright.style.top = '0';
+}
+
+function close(){
+    nbright.style.top = '-100%';
+}
+
+
+
 document.getElementById('suggestion').onkeyup = function(){
     if(document.querySelector('#name-input').value.length>0 && document.querySelector('#suggestion').value.length>0 ){
         document.querySelector('#name-submit').disabled=false;
